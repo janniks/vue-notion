fea3d392372a357773c7638498c1fd59b9124fcc
janniks <jannik.siebert@gmail.com>
2020-12-17 13:44:12 +0100

    Add quote support

---
+++ src/blocks/quote.vue
@@ -0,0 +1,16 @@
+<template>
+  <blockquote v-if="properties" class="notion-quote">
+    <NotionTextRenderer :title="title" />
+  </blockquote>
+</template>
+
+<script>
+import Blockable from "@/lib/blockable";
+import NotionTextRenderer from "@/blocks/helpers/text-renderer";
+
+export default {
+  extends: Blockable,
+  name: "NotionText",
+  components: { NotionTextRenderer },
+};
+</script>
