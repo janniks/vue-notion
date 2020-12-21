44084327f0d08a09f8d9603987fb51e6e4998db8
janniks <jannik.siebert@gmail.com>
2020-12-21 10:57:27 +0100

    Update postpublish script

---
+++ scripts/postpublish.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+GIT_TAG=$(git describe --abbrev=0 --tags)
+
+$(cd example && npm install -f vue-notion@latest)
+
+git add --all
+git commit -m 'Update example'
+git push --tags
+
+gh release create $GIT_TAG -t "$GIT_TAG"
