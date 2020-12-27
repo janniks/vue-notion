f81939a0ebe8f54747a9ca87ed3d86f2251e0be8
janniks <jannik.siebert@gmail.com>
2020-12-27 20:42:22 +0100

    Refactor example

--- nuxt/index.js
+++ nuxt/index.js
@@ -1,24 +1,27 @@
-import path from 'path'
-import defu from 'defu'
+import path from "path";
+import defu from "defu";
 
-const defaultOptions = {}
+const defaultOptions = {};
 
-module.exports = function (moduleOptions) {
-  const options = defu({
-    ...this.options.notion,
-    ...moduleOptions
-  }, defaultOptions)
+module.exports = function(moduleOptions) {
+  const options = defu(
+    {
+      ...this.options.notion,
+      ...moduleOptions,
+    },
+    defaultOptions
+  );
 
-  this.nuxt.hook('build:before', () => {
+  this.nuxt.hook("build:before", () => {
     // Enable transpilation for `vue-notion`
-    this.options.build.transpile.push('vue-notion')
+    this.options.build.transpile.push("vue-notion");
 
     this.addPlugin({
-      src: path.resolve(__dirname, 'plugin.js'),
-      fileName: 'vue-notion.js',
-      options
-    })
-  })
-}
+      src: path.resolve(__dirname, "plugin.js"),
+      fileName: "vue-notion.js",
+      options,
+    });
+  });
+};
 
-module.exports.meta = require('../package.json')
+module.exports.meta = require("../package.json");
