5d612acde8bf7343068d0f0ea3a82444b37e60cc
janniks <jannik.siebert@gmail.com>
2021-09-04 17:25:45 +0200

    Remove start from nested lists

--- src/blocks/helpers/nested-list.vue
+++ src/blocks/helpers/nested-list.vue
@@ -2,7 +2,7 @@
   <ul v-if="type === 'bulleted_list'" class="notion-list notion-list-disc">
     <slot />
   </ul>
-  <ol v-else class="notion-list notion-list-numbered" :start="start">
+  <ol v-else class="notion-list notion-list-numbered">
     <slot />
   </ol>
 </template>
