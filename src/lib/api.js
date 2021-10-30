38bb47faf648ba90dc9afc68dc1bf717f1b80d26
janniks <jannik.siebert@gmail.com>
2021-10-30 19:13:35 +0200

    Add experimental path

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,12 +2,12 @@ import fetch from "cross-fetch";
 
 const getPageTable = async (
   pageId,
-  apiUrl = "https://api.vue-notion.workers.dev/v1"
+  apiUrl = "https://experimental.vue-notion.workers.dev/v1"
 ) => await fetch(`${apiUrl}/table/${pageId}`).then((res) => res.json());
 
 const getPageBlocks = async (
   pageId,
-  apiUrl = "https://api.vue-notion.workers.dev/v1"
+  apiUrl = "https://experimental.vue-notion.workers.dev/v1"
 ) => await fetch(`${apiUrl}/page/${pageId}`).then((res) => res.json());
 
 export { getPageTable, getPageBlocks };
