9438f7f4226999f7e0814f8b79502ed9619dde59
elias.a.pintos.arias <elias.pintos.aris@gmail.com>
2022-02-09 01:12:55 +0000

    Tweaks image component to receive custom props

--- src/blocks/helpers/image.vue
+++ src/blocks/helpers/image.vue
@@ -1,23 +1,31 @@
 <template>
   <div v-if="f.block_aspect_ratio" :style="style">
-    <img class="notion-image-inset" :alt="alt || 'Notion image'" :src="src" />
+    <component v-if="hasImageComponent" :is="imageOptions.component" class="notion-image-inset" :alt="alt || 'Notion image'" :src="src" v-bind="attrs"/>
+    <img v-else class="notion-image-inset" :alt="alt || 'Notion image'" :src="src" v-bind="attrs" />
   </div>
-  <img v-else :alt="caption" :src="src" />
+  <component v-else-if="hasImageComponent" :is="imageOptions.component" :alt="alt || 'Notion image'" :src="src" v-bind="attrs" />
+  <img v-else :alt="caption" :src="src" v-bind="attrs" />
 </template>
 
 <script>
-import Blockable, { blockComputed } from "@/lib/blockable";
+import Blockable from "@/lib/blockable";
 
 export default {
   extends: Blockable,
   name: "NotionImage",
   computed: {
-    ...blockComputed,
     alt() {
       return this.caption?.[0][0];
     },
     src() {
       return this.mapImageUrl(this.properties?.source[0][0], this.block);
+    },
+    attrs() {
+      const {component, ...attrs} = this.imageOptions || {}
+      return attrs;  
+    },
+    hasImageComponent() {
+      return !!this.imageOptions?.component;
     },
     style() {
       const aspectRatio =
