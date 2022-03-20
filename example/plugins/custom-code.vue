82bd1cb2220175406702325f53a6066303ec0ba5
janniks <janniks@users.noreply.github.com>
2022-03-20 19:34:46 +0100

    update exports

--- example/plugins/custom-code.vue
+++ example/plugins/custom-code.vue
@@ -1,42 +1,30 @@
 <template>
   <pre
-    :class="['notion-code', langClass]"
+    class="notion-code-wrapper"
     :style="{
       border: '3px solid tomato',
     }"
   >
-    <PrismComponent v-if="prism && supported" :language="lang">{{ properties.title[0][0] }}</PrismComponent>
-    <code v-else :class="langClass">{{ properties.title[0][0] }}</code>
+    <NotionCode v-bind="pass"/>
   </pre>
 </template>
 
 <script>
-import Prism from "prismjs";
-import PrismComponent from "vue-prism-component";
-
-import { Blockable, blockComputed, blockProps } from "vue-notion";
+import { NotionCode, Blockable, blockComputed, blockProps } from "vue-notion";
 
 export default {
   extends: Blockable,
   name: "CustomCode",
-  components: { PrismComponent },
-  props: { ...blockProps, overrideLang: String, overrideLangClass: String },
-  data() {
-    return { Prism };
+  components: {
+    NotionCode,
+  },
+  props: {
+    ...blockProps,
+    //> your own additional props
   },
   computed: {
     ...blockComputed,
-    lang() {
-      return (
-        this.overrideLang || this.properties?.language?.[0]?.[0]?.toLowerCase()
-      );
-    },
-    langClass() {
-      return this.overrideLangClass || `language-${this.lang}`;
-    },
-    supported() {
-      return this.Prism.languages[this.lang];
-    },
+    //> your own additional `computed` methods go here
   },
 };
 </script>
