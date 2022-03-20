134114c18cdf26b373872d14330525dfa90e0a1c
janniks <janniks@users.noreply.github.com>
2022-03-20 18:20:06 +0100

    add custom component

---
+++ example/plugins/custom-code.vue
@@ -0,0 +1,43 @@
+<template>
+  <pre
+    :style="{
+      border: '3px solid tomato',
+    }"
+    v-if="prism && supported"
+    :class="['notion-code', langClass]"
+  ><PrismComponent :language="lang">{{ properties.title[0][0] }}</PrismComponent></pre>
+  <pre
+    v-else
+    :class="['notion-code', langClass]"
+  ><code :class="langClass">{{ properties.title[0][0] }}</code></pre>
+</template>
+
+<script>
+import Prism from "prismjs";
+import PrismComponent from "vue-prism-component";
+import { Blockable, blockComputed, blockProps } from "vue-notion";
+
+export default {
+  extends: Blockable,
+  name: "CustomCode",
+  components: { PrismComponent },
+  props: { ...blockProps, overrideLang: String, overrideLangClass: String },
+  data() {
+    return { Prism };
+  },
+  computed: {
+    ...blockComputed,
+    lang() {
+      return (
+        this.overrideLang || this.properties?.language?.[0]?.[0]?.toLowerCase()
+      );
+    },
+    langClass() {
+      return this.overrideLangClass || `language-${this.lang}`;
+    },
+    supported() {
+      return this.Prism.languages[this.lang];
+    },
+  },
+};
+</script>
