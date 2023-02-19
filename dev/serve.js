9a7e073ed52fc44bbcd8abd5cf7eb828859dca50
janniks <janniks@users.noreply.github.com>
2023-02-19 17:41:58 +0100

    Update alpha

--- dev/serve.js
+++ dev/serve.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./serve.vue";
 
-import Vue3Katex from "@hsorby/vue3-katex";
+import Vue3Katex from "@janniks/vue3-katex";
 import { NotionRenderer } from "@/entry";
 
 const app = createApp(App);
