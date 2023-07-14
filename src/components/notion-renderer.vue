4fa04609e8ee2a15b06035dc5f09ecb9e01b7220
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-07-14 19:46:53 +0200

    Navigate to block

--- src/components/notion-renderer.vue
+++ src/components/notion-renderer.vue
@@ -1,7 +1,7 @@
 <template>
   <NotionBlock v-bind="pass" v-if="blockMap && value">
     <NotionRenderer v-for="(contentId, contentIndex) in value.content" v-bind="pass" :key="contentId" :level="level + 1"
-      :content-id="contentId" :content-index="contentIndex" />
+      :content-id="contentId" :id="contentId" :content-index="contentIndex" />
   </NotionBlock>
 </template>
 
