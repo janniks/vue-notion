b2ef385d30f50b20c06439be0b306c8b06052249
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-11-13 17:21:07 +0100

    Fix props error

--- blocks/table-of-contents.vue
+++ blocks/table-of-contents.vue
@@ -9,51 +9,51 @@
 
 <script>
   import { Blockable } from '../lib/blockable';
+  import { computed } from 'vue';
+  import TableOfContentsItem from './helpers/table-of-contents-item.vue';
 
   export default {
     name: 'NotionTableOfContents',
     extends: Blockable,
+    setup(props) {
+      const headings = computed(() => {
+        const output = {};
+        Object.keys(props.blockMap).forEach((key) => {
+          if (props.blockMap[key].value.type.includes('header')) {
+            output[key] = props.blockMap[key];
+          }
+        });
+        return output;
+      });
+
+      const toc = computed(() => {
+        return Object.keys(headings.value).map((key) => {
+          const block = props.blockMap[key].value;
+          return {
+            title: block.properties.title.flat(100).join(' '),
+            level: block.type
+              .split('_')
+              .reduce((acc, value) => (value === 'sub' ? acc + 1 : acc), 1),
+            id: block.id,
+            sub: [],
+          };
+        });
+      });
+
+      const hierarchy = computed(() => {
+        const stack = toc.value.slice(0).reverse();
+
+        stack.forEach((kid) => {
+          const parentIndex = stack.findIndex(
+            (parent) => parent.level < kid.level,
+          );
+          if (parentIndex) {
+            stack[parentIndex].sub = [...stack[parentIndex].sub, stack.shift()];
+          }
+        });
+
+        return stack.reverse();
+      });
+    },
   };
-</script>
-
-<script setup>
-  import { computed } from 'vue';
-  import TableOfContentsItem from './helpers/table-of-contents-item.vue';
-
-  const headings = computed(() => {
-    const output = {};
-    Object.keys(props.blockMap).forEach((key) => {
-      if (props.blockMap[key].value.type.includes('header')) {
-        output[key] = props.blockMap[key];
-      }
-    });
-    return output;
-  });
-
-  const toc = computed(() => {
-    return Object.keys(headings.value).map((key) => {
-      const block = props.blockMap[key].value;
-      return {
-        title: block.properties.title.flat(100).join(' '),
-        level: block.type
-          .split('_')
-          .reduce((acc, value) => (value === 'sub' ? acc + 1 : acc), 1),
-        id: block.id,
-        sub: [],
-      };
-    });
-  });
-
-  const hierarchy = computed(() => {
-    const stack = toc.value.slice(0).reverse();
-
-    stack.forEach((kid) => {
-      const parentIndex = stack.findIndex((parent) => parent.level < kid.level);
-      if (parentIndex) {
-        stack[parentIndex].sub = [...stack[parentIndex].sub, stack.shift()];
-      }
-    });
-
-    return stack.reverse();
-  });
 </script>
