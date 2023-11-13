d7a6b79b230643c0bfc7f22648910062158f8831
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-11-13 16:35:43 +0100

    Add formatters

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: true,
+  arrowParens: 'always',
+  htmlWhitespaceSensitivity: 'css',
+  vueIndentScriptAndStyle: true,
+};
